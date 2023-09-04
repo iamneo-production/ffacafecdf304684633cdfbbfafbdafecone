0110967a0d1907810c47dfbfae34e27fd57889c4
Iamneo <techadmin@iamneo.ai>
2023-09-04 04:09:12 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+import CreateArea from './components/CreateArea';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Note from './components/Note';
+
+function App() {
+  // Enter your code here
+}
+
+export default App;
